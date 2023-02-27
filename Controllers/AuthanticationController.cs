19b9936890260485441f4c3eeb228365a70e473f
Harpesh Patel <110534999+harpesh2001@users.noreply.github.com>
2023-02-27 18:23:25 +0530

    Add project files.

---
+++ Controllers/AuthanticationController.cs
@@ -0,0 +1,69 @@
+﻿using CI_Platform_three_tier.DataModels.DataModels;
+using CI_Platform_three_tier.Repository.Repository.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+/*using CI_Platform_three_tier.Repository.Interface;
+*/
+
+
+namespace CI_Platform_three_tier.Controllers
+{
+
+
+
+    public class AuthanticationController : Controller
+    {
+
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<AuthanticationController> _logger; 
+
+        public AuthanticationController( ILogger<AuthanticationController> logger, IUserRepository userRepository )
+        {
+            _logger= logger;    
+            _userRepository= userRepository;
+
+        }
+
+
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("/Authantication/Login", Name = "Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Authantication/Register", Name = "CreateUserPost")]
+        public async Task<IActionResult> Register(User model)
+        {
+            try {
+                await _userRepository.RegisterUserAsync(model);
+                return RedirectToRoute("Login");
+            }
+            catch(Exception ex) {
+                //asdfa
+                return RedirectToRoute("Login");
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult ResetPassword() {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ForgotPassword() 
+        {
+            return View();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using CI_Platform_three_tier.DataModels.DataModels;
+using CI_Platform_three_tier.Repository.Repository;
+using CI_Platform_three_tier.Repository.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+/*
+using CI_Platform_three_tier.Repository.;*/
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<PlatformDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
