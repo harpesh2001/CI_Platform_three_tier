8e033027af62f478e26da3ddb3db2d868f9aefcc
Harpesh Patel <110534999+harpesh2001@users.noreply.github.com>
2023-02-28 18:23:15 +0530

    Login Page Integration

--- Controllers/AuthanticationController.cs
+++ Controllers/AuthanticationController.cs
@@ -34,6 +34,32 @@ namespace CI_Platform_three_tier.Controllers
         {
             return View();
         }
+
+
+        [HttpPost]
+        [Route("", Name ="Default")]
+        [Route("/Authentication/Login", Name ="LoginPost")]
+        public IActionResult Login(User model)
+        {
+            /*if (ModelState.IsValid)
+            {
+               */ var validity = _userRepository.VerifyUserAsync(model);
+                if (validity == 0)
+                {
+                    ViewBag.msg = 0;
+                    
+                }
+                else
+                {
+                    return Redirect("/Home/Index");
+                }
+            return View(model);
+           /* }
+            ViewData["ModelState"] = "Model state invalid.";
+            return View(model);*/
+            
+        }
+
         [HttpGet]
         [AllowAnonymous]
         public IActionResult Register()
@@ -45,12 +71,12 @@ namespace CI_Platform_three_tier.Controllers
         [Route("/Authantication/Register", Name = "CreateUserPost")]
         public async Task<IActionResult> Register(User model)
         {
-            try {
+            if(ModelState.IsValid) { 
                 await _userRepository.RegisterUserAsync(model);
                 return RedirectToRoute("Login");
             }
-            catch(Exception ex) {
-                //asdfa
+            else
+            {
                 return RedirectToRoute("Login");
             }
         }
@@ -64,6 +90,32 @@ namespace CI_Platform_three_tier.Controllers
         public IActionResult ForgotPassword() 
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        
+        public IActionResult ForgotPassword(User user)
+        {
+            var validity = _userRepository.VerifyUserAsync(user);
+            if (validity == 0)
+            {
+                ViewBag.msg = 0;
+            }
+            else
+            {
+                var otp = generateOtp();
+
+            }
+            return View();
+        }
+
+        private string generateOtp()
+        {
+
+            Random r = new Random();
+            string otp = (r.Next(100000, 999999)).ToString();
+            return otp;
         }
     }
 }
